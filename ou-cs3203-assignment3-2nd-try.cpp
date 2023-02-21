2eab4af50d0e00b6743011d2167ad8479f06fc84
Abe Artus <abraham.a.artus-1@ou.edu>
2023-02-21 13:27:36 -0600

    Multiiply fjunction Added/Implemented

--- ou-cs3203-assignment3-2nd-try.cpp
+++ ou-cs3203-assignment3-2nd-try.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int sumList(vector<int> arr);
+int multiplyList(vector<int> arr);
 
 // computes the sum of a list / array of numbers given as a parameter and returns the result.
 int sumList(vector<int> arr ) {
@@ -12,4 +13,15 @@ int sumList(vector<int> arr ) {
         sum += arr[i];
     }
     return sum;
+}
+
+
+// compute the multiple of all the numbers in the list
+int multiplyList(vector<int> arr) {
+    int prod = 1;
+    // multyply each elemnt times prod and save to product
+    for (int i = 0; i < arr.size()  ; i++) {
+        prod = arr[i]*prod;
+    }
+    return prod;
 }
