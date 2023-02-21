fe4a1713d229d2ce146329d737fdd4f4d8a54588
Abe Artus <abraham.a.artus-1@ou.edu>
2023-02-21 13:33:04 -0600

    Main function implemented /  print func added

--- ou-cs3203-assignment3-2nd-try.cpp
+++ ou-cs3203-assignment3-2nd-try.cpp
@@ -4,6 +4,36 @@ using namespace std;
 
 int sumList(vector<int> arr);
 int multiplyList(vector<int> arr);
+void printvec(vector<int> arr);
+
+int main() {
+    // init vector to be used
+    vector<int> list;  
+
+    // prompt user to enter Integers
+    cout << "Enter vectors numbers seperated by spaces, end seq. with E:";
+    int n;
+    while (cin >> n) {  
+        list.push_back(n);  
+    }
+    
+    // display the vector size and elements
+    cout<< "Inputted Vector: ";
+    printvec(list);
+
+    // sum of the array
+    cout << "Sum of the Vector Integers: " << sumList(list) << endl;
+
+    // vector multiplied together
+    cout << "Product of the Vector Integers: " << multiplyList(list) << endl;
+}
+
+void printvec(vector<int> arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 
 // computes the sum of a list / array of numbers given as a parameter and returns the result.
 int sumList(vector<int> arr ) {
